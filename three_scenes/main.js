cba9d69e2889b5a8920af6dc490e8bff51b6f697
Kristian Hristakiev <khristakiev@gmail.com>
2026-03-15 01:39:23 +0200

    finish 3d models for presentation

--- three_scenes/main.js
+++ three_scenes/main.js
@@ -1,14 +1,16 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import * as MODELS from './models.js'
+import { FontLoader } from 'three/addons/loaders/FontLoader.js';
+import { gsap } from "gsap";
 
 
 const container = document.getElementById("scene-container");
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 60, container.clientWidth / container.clientHeight, 0.1, 1000 );
-camera.position.set(0,10,0)
-camera.lookAt(0,0,0);
+camera.position.set(0,15,0)
+//camera.lookAt(0,15,5);
 
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize( container.clientWidth , container.clientHeight );
@@ -26,24 +28,37 @@ const light = new THREE.PointLight('white', 1200);
 light.position.set(-15, 10, 5);
 scene.add(light);
 
-let token = MODELS.get_token(2, new THREE.Color("silver"));
+let token = MODELS.get_token(new THREE.Color("silver"));
 scene.add( token );
 token.position.x = 10
 
 
 const laptop = MODELS.get_laptop();
 laptop.scale.set(3,3,3)
+laptop.position.z = -10
 scene.add(laptop)
 
-//scene.add(new THREE.Mesh(new THREE.BoxGeometry(), new THREE.MeshBasicMaterial({color: "red"})))
-        
+const loader = new FontLoader();
+const font = await loader.loadAsync('resource/Roboto_SemiCondensed_SemiBold_Regular.json');
+const cloud = MODELS.get_cloud(font, "Hello, world");
+scene.add(cloud);
+cloud.position.set(-7,0,0)
+
+let serverRack = MODELS.get_server_rack()
+serverRack.position.set(-15,0,0);
+scene.add(serverRack)
+
+let press = MODELS.get_token_press();
+// press.position.set(0,0,5)
+// press.rotation.x = -Math.PI/2
+MODELS.animate_token_press(press)
+scene.add(press)
+
+       
 
 camera.position.z = 5;
 
 function animate( time ) {
-
-  //token.rotation.x = time / 2000;
-  //token.rotation.y = time / 1000;
 
   controls.update();
   renderer.render( scene, camera );
--- three_scenes/models.js
+++ three_scenes/models.js
@@ -1,10 +1,10 @@
 import * as THREE from 'three';
 import { RoundedBoxGeometry } from 'three/addons/geometries/RoundedBoxGeometry.js';
-// import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { TextGeometry } from 'three/addons/geometries/TextGeometry.js';
+import { gsap } from "gsap";
 
-// const loader = new GLTFLoader();
-
-export function get_token(radius, color) {
+export function get_token(color) {
+    const radius = 1;
     const tokenMaterial = new THREE.MeshPhongMaterial();
     tokenMaterial.color = color;
     const tokenMaterial2 = new THREE.MeshPhongMaterial();
@@ -13,7 +13,7 @@ export function get_token(radius, color) {
 
     const token = new THREE.Mesh(tokenGeom, tokenMaterial);
 
-    const tPart = new THREE.Mesh(new THREE.BoxGeometry(radius, radius / 2, radius / 5), tokenMaterial2);
+    const tPart = new THREE.Mesh(new THREE.BoxGeometry(radius, 0.35, radius / 5), tokenMaterial2);
 
     const tPart2 = tPart.clone()
 
@@ -25,43 +25,219 @@ export function get_token(radius, color) {
 
     return token;
 }
+
 export function get_laptop() {
-    const laptopBodyMaterial = new THREE.MeshPhongMaterial({color: "#2f3036"})
+    const laptopBodyMaterial = new THREE.MeshPhongMaterial({ color: "#2f3036" })
     const laptopBodyGeom = new RoundedBoxGeometry(1.6, 0.05, 1)
     let laptopBodyMesh = new THREE.Mesh(laptopBodyGeom, laptopBodyMaterial);
 
     let screen = laptopBodyMesh.clone()
 
-    screen.rotation.x = Math.PI/2
-    screen.position.set(0,0.5,-0.5)
+    screen.rotation.x = Math.PI / 2
+    screen.position.set(0, 0.5, -0.5)
     let display = screen.clone();
     display.material = new THREE.MeshPhongMaterial({
         color: "white",
         emissive: "#e6e4ff"
     })
     display.scale.set(0.92, 0.92, 0.92);
-    display.position.z+=0.01
+    display.position.z += 0.01
     laptopBodyMesh.add(screen, display);
 
-    const buttonMaterial = new THREE.MeshPhongMaterial({color: "#676767"})
+    const buttonMaterial = new THREE.MeshPhongMaterial({ color: "#676767" })
     const button = new THREE.Mesh(new THREE.BoxGeometry(0.06, 0.06, 0.05), buttonMaterial)
 
-    for(let i = -7; i <= 7; i++)
-    {
+    for (let i = -7; i <= 7; i++) {
         let b1 = button.clone();
-        b1.position.set(i*0.07, 0.02, -0.25);
+        b1.position.set(i * 0.07, 0.02, -0.25);
         let b2 = b1.clone();
-        b2.position.z+=0.1
+        b2.position.z += 0.1
         let b3 = b2.clone();
-        b3.position.z+=0.1
+        b3.position.z += 0.1
         let b4 = b3.clone();
-        b4.position.z+=0.1
-        laptopBodyMesh.add(b1,b2,b3,b4);
+        b4.position.z += 0.1
+        laptopBodyMesh.add(b1, b2, b3, b4);
     }
 
     const trackpad = new THREE.Mesh(new THREE.BoxGeometry(0.4, 0.05, 0.2), buttonMaterial)
-    trackpad.position.set(0,0.003,0.3)
+    trackpad.position.set(0, 0.003, 0.3)
     laptopBodyMesh.add(trackpad)
 
     return laptopBodyMesh;
+}
+
+export function get_cloud(font, txt) {
+    const textGeometry = new TextGeometry(txt, {
+        font: font,
+        size: 1.5,
+        depth: 0.25,
+        curveSegments: 12
+    });
+    const textMaterial = new THREE.MeshPhongMaterial({
+        color: "yellow"
+    })
+    const text = new THREE.Mesh(textGeometry, textMaterial);
+    text.position.z = 0.2;
+    textGeometry.computeBoundingBox();
+    const boundingBox = textGeometry.boundingBox;
+    const width = boundingBox.max.x - boundingBox.min.x;
+    text.position.x = -width / 2
+
+    const cloudMaterial = new THREE.MeshPhongMaterial({
+        color: "#e0f7ff",
+        emissive: "#ffffff"
+    })
+
+    const result = new THREE.Group();
+    const cloud = new THREE.Mesh(new THREE.BoxGeometry(5, 1.4, 0.7), cloudMaterial);
+    cloud.position.y = 0.2;
+    const circlePart = new THREE.Mesh(new THREE.CylinderGeometry(1, 1, 0.7), cloudMaterial);
+    circlePart.rotation.x = Math.PI / 2;
+    circlePart.position.set(-2.5, 0.5, 0);
+
+    const c2 = circlePart.clone();
+    c2.scale.set(1.5, 1, 1.5);
+    c2.position.set(2.5, 1, 0)
+
+    const c3 = circlePart.clone()
+    c3.scale.set(1.3, 1, 1.3);
+    c3.position.set(1, 2, 0);
+
+    const c4 = circlePart.clone()
+    c4.scale.set(1.1, 1, 1.1);
+    c4.position.set(-1.5, 1.3, 0);
+
+    const c5 = circlePart.clone()
+    c5.scale.set(1.25, 1, 1.25);
+    c5.position.set(-0.5, 1.7, 0);
+
+    result.add(circlePart, c2, c3, c4, c5, cloud)
+    result.add(text);
+
+    return result;
+}
+
+export function get_server_rack() {
+    const serverMaterial1 = new THREE.MeshStandardMaterial({
+        color: "grey",
+        metalness: 0.9
+    })
+    const serverMaterial2 = new THREE.MeshPhongMaterial({
+        color: "#222222"
+    })
+    const serverRack = new THREE.Mesh(new THREE.BoxGeometry(3, 6.5, 3), serverMaterial1);
+
+    const piece = new THREE.Mesh(new THREE.BoxGeometry(2.5, 0.5, 1), serverMaterial2)
+    const serverLight = new THREE.Mesh(new THREE.BoxGeometry(0.2, 0.2, 0.2), new THREE.MeshBasicMaterial({ color: "red" }));
+    piece.add(serverLight);
+    serverLight.position.set(1.15, 0.1, 0.5);
+
+    for (let i = 0; i < 10; i++) {
+        let p = piece.clone();
+        p.position.set(0, 2.8 - i * 0.6, 1.05)
+        serverRack.add(p);
+    }
+
+    return serverRack;
+}
+
+export function get_token_press() {
+    const pressMaterial1 = new THREE.MeshPhongMaterial({
+        color: "#3b4200"
+    })
+    const pressMaterial2 = new THREE.MeshPhongMaterial({
+        color: "silver"
+    })
+    const pressMaterial3 = new THREE.MeshPhongMaterial({
+        color: "#d5d5d5",
+        shininess: 0.9,
+        specular: "blue"
+    })
+
+    const tokenPress = new THREE.Group();
+
+    const base = new THREE.Mesh(new THREE.BoxGeometry(3, 1, 3), pressMaterial1);
+    const base2 = base.clone();
+    base2.rotation.x = Math.PI / 2;
+    base2.position.set(-0.5, 2, -1.5);
+
+    const base3 = base2.clone();
+    base3.position.set(0.5, 2, 1.5);
+
+    const base4 = base.clone();
+    base4.rotation.z = Math.PI / 2;
+    base4.position.set(1.5, 2, -0.5)
+
+    const base5 = base.clone();
+    base5.rotation.z = Math.PI / 2;
+    base5.position.set(-1.5, 2, 0.5)
+
+    base.scale.set(4 / 3, 1, 4 / 3);
+    base.position.set(0, 0.5, 0)
+
+    tokenPress.add(base, base2, base3, base4, base5)
+
+    const plate = new THREE.Mesh(new THREE.BoxGeometry(2, 2, 2), pressMaterial2);
+    plate.position.y = 3.5
+    plate.name = "plate"
+    tokenPress.add(plate)
+
+    const pole1 = new THREE.Mesh(new THREE.BoxGeometry(0.5, 9.5, 0.5), pressMaterial2)
+    pole1.position.set(-2.25, 4.75, 1.5);
+    const pole2 = pole1.clone();
+    pole1.position.z = -1.5;
+    tokenPress.add(pole1, pole2)
+
+    const top = base.clone();
+    top.position.y = 9
+    tokenPress.add(top)
+
+    const press = new THREE.Mesh(new THREE.BoxGeometry(2.5, 0.5, 2.5), pressMaterial3);
+    press.position.y = 7.75
+    press.name = "press"
+    tokenPress.add(press)
+
+    const rod = new THREE.Mesh(new THREE.BoxGeometry(0.5, 0.5, 0.5), pressMaterial2);
+    rod.position.y = 8.25
+    rod.name = "rod"
+    tokenPress.add(rod)
+
+    const tokenColor = new THREE.Color("#897813")
+    const token = get_token(tokenColor)
+    token.position.y = 4.6
+    token.name = "token"
+    token.scale.set(0.9, 0.9, 0.9)
+    tokenPress.add(token)
+
+    const rawToken = new THREE.Mesh(new THREE.CylinderGeometry(0.9, 0.9, 0.36), new THREE.MeshPhongMaterial({ color: tokenColor }))
+    rawToken.position.y = 4.6;
+    rawToken.name = "rawToken"
+    tokenPress.add(rawToken)
+
+
+    return tokenPress;
+}
+
+export function animate_token_press(tokenPress) {
+    let tl1 = gsap.timeline({
+        repeat: -1
+    });
+
+    tl1.to(tokenPress.getObjectByName("press").position, { y: 4.9, duration: 1.5, ease: "sine.inOut" })
+        .to(tokenPress.getObjectByName("rod").scale, { y: 6.75, duration: 1.5, ease: "sine.inOut" }, "0")
+        .to(tokenPress.getObjectByName("rod").position, { y: 6.85, duration: 1.5, ease: "sine.inOut" }, "0")
+        .to(tokenPress.getObjectByName("rawToken"), { visible: false })
+        .add("lift", "+=0.1")
+        .to(tokenPress.getObjectByName("press").position, { y: 7.75, duration: 1.5, ease: "sine.inOut" }, "lift")
+        .to(tokenPress.getObjectByName("rod").scale, { y: 1, duration: 1.5, ease: "sine.inOut" }, "lift")
+        .to(tokenPress.getObjectByName("rod").position, { y: 8.25, duration: 1.5, ease: "sine.inOut" }, "lift")
+        .add("plateGoesDown", "-=0.75")
+        .to(tokenPress.getObjectByName("plate").position, { y: 1, duration: 2, ease: "back.in" }, "plateGoesDown")
+        .to(tokenPress.getObjectByName("token").position, { y: 1.8, duration: 2.24, ease: "back.in" }, "plateGoesDown")
+        .to(tokenPress.getObjectByName("rawToken").position, { y: 1.8, duration: 2 }, "plateGoesDown")
+        .to(tokenPress.getObjectByName("rawToken"), { visible: true })
+        .to(tokenPress.getObjectByName("token"), { visible: false })
+        .add("plateGoesUp")
+        .to(tokenPress.getObjectByName("plate").position, { y: 3.5, duration: 1.5, ease: "back.out" }, "plateGoesUp")
+        .to(tokenPress.getObjectByName("rawToken").position, { y: 4.6, duration: 1.5, ease: "back.out" }, "plateGoesUp+0.1")
 }
